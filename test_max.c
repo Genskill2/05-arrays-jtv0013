36a02a78f5361616cad399938484388d8b46266f
Jose Tom Vettom <84711737+jtv0013@users.noreply.github.com>
2021-06-17 19:56:44 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -3,18 +3,6 @@
 
 
 int max(int [], int);
-int max(int k[],x)
-{
-int t=k[0];
-for(int i=0;i++;i<x)
-{
-if(k[i+1]>k[i])
-{
-t=k[i+1);
-return(t);
-}
-}
-}
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
