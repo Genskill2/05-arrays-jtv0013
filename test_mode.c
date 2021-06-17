3ad15448f3cc0cacdaf12288c35f7945d64d9355
Jose Tom Vettom <84711737+jtv0013@users.noreply.github.com>
2021-06-17 11:52:43 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -3,6 +3,26 @@
 
 
 int mode(int [], int);
+int mode(int x[], int n)
+{
+int max=0,most=0,i,j;
+
+for (i = 0; i < n; ++i) {
+int count = 0;
+      
+for (j = 0; j < n; ++j) {
+if (x[j] == x[i])
+++count;
+}
+      
+if (count > most) {
+most = count;
+max= x[i];
+}
+   }
+
+return(max);
+}
 
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
